64c38142e1a7a88759ceb95e8e791f50f37902b9
carlosmaxy32 <carlos.moreno6564@alumnos.udg.mx>
2022-09-07 22:10:33 -0500

    11. Cargando a la base de datos los nuevos alumnos e instalación de seet alert 2

--- src/app/students/form.component.ts
+++ src/app/students/form.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Student } from './student';
-
+import { StudentService } from './student.service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-form',
-  templateUrl: './form.component.html'
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
 
   public student: Student = new Student();
   public title: string = "Crear Alumno"
-  constructor() { }
+  constructor(private studentService: StudentService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
   public create(): void {
-    console.log("Clicked")
-    console.log(this.student)
+    this.studentService.create(this.student).subscribe(
+      student=> {
+        this.router.navigate(['/students']);
+        Swal.fire('Estudiante nuevo', `Se añadió al estudiante ${student.name} ${student.lastname} con éxito`,'success');
+      }
+    )
+
   }
 
 }
--- src/app/students/student.service.ts
+++ src/app/students/student.service.ts
@@ -2,18 +2,22 @@ import { Injectable } from '@angular/core';
 import { STUDENTS } from './students.json';
 import { Student } from './student';
 import { of, Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
   private urlEndPoint:string = "http://localhost:8080/api/students";
-
+ private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
   constructor(private http: HttpClient) { }
   getStudents(): Observable<Student[]> {
     return this.http.get(this.urlEndPoint).pipe(
       map((response) => response as Student[])
     );
+  }
+
+  create(student: Student) : Observable<Student> {
+    return this.http.post<Student>(this.urlEndPoint, student, {headers: this.httpHeaders})
   }
 }
